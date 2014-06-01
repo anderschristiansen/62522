e2ef64702fed9ead0d585c2b3102cc8c7b1593d1
mlj <m_jensen3@hotmail.com>
2014-06-02 00:38:46 +0200

    ..

--- RoskildeApp/src/com/roskildeapp/SettingsActivity.java
+++ RoskildeApp/src/com/roskildeapp/SettingsActivity.java
@@ -15,13 +15,14 @@ public class SettingsActivity extends PreferenceActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.layout.activity_settings);
+		PreferenceManager.getDefaultSharedPreferences(this).getBoolean("Skift tema", false);
+
 		SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
 
 		if(settings.getBoolean("skift tema", true)){
 			setActivityBackgroundRecource(R.color.orange);
 		}
 		
-		PreferenceManager.getDefaultSharedPreferences(this).getBoolean("Skift tema", false);
 	}
 
 	@Override
